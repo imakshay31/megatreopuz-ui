2d8aecd903e72ff2380f0732187b8a4b533e55d6
Yash Mahalwal <yashmahalwal@gmail.com>
2020-06-13 14:34:40 +0530

    feat: basic setup done

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2020: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 11,
+        sourceType: "module",
+    },
+    plugins: ["react", "@typescript-eslint"],
+    rules: {
+        "react/display-name": 0,
+        "react/prop-types": 0,
+    },
+    ignorePatterns: ["*.js", "*.d.ts"],
+};
